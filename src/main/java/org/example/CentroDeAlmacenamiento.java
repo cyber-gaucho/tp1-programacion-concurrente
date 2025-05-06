4d9a31713e7a24b50b5c492b314641134d7e1095
Valentino <valentinorenaudo@gmail.com>
2025-05-06 11:12:09 -0300

    Versión 5: se agregaron imágenes de diagramas, el informe en PDF y mejoras en el código

--- src/main/java/org/example/CentroDeAlmacenamiento.java
+++ src/main/java/org/example/CentroDeAlmacenamiento.java
@@ -21,41 +21,6 @@ public class CentroDeAlmacenamiento {
         this.nombre = nombreCentro;
     }
 
-    // public synchronized Casillero obtenerCasillero() throws noHayCasillerosDisponiblesException {
-    //     for (int i = 0; i < this.matriz.length; i++) {
-    //         for (int j = 0; j < this.matriz[i].length; j++) {
-    //             if (this.matriz[i][j].getEstado() == EstadoCasillero.VACIO) {
-    //                 return this.matriz[i][j];
-    //             }
-    //         }
-    //     }
-    //     throw new noHayCasillerosDisponiblesException();
-    // }
-
-    // public synchronized Casillero obtenerCasillero() throws noHayCasillerosDisponiblesException {
-    //     List<Casillero> todos = new ArrayList<>();
-
-    //     // Pasar la matriz a una lista
-    //     for (int i = 0; i < this.matriz.length; i++) {
-    //         for (int j = 0; j < this.matriz[i].length; j++) {
-    //             todos.add(this.matriz[i][j]);
-    //         }
-    //     }
-
-    //     // Mezclar aleatoriamente
-    //     Collections.shuffle(todos);
-
-    //     // Buscar el primero disponible
-    //     for (Casillero c : todos) {
-    //         if (c.getEstado() == EstadoCasillero.VACIO) {
-    //             return c;
-    //         }
-    //     }
-
-    //     // Si no se encuentra ninguno
-    //     throw new noHayCasillerosDisponiblesException();
-    // }
-
     public synchronized Casillero obtenerCasillero() throws noHayCasillerosDisponiblesException {
         int filas = matriz.length;
         int columnas = matriz[0].length;
@@ -83,10 +48,10 @@ public class CentroDeAlmacenamiento {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(nombre + ": \n");
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                sb.append(this.matriz[i][j]);
+        sb.append(nombre).append(": \n");
+        for (Casillero[] casilleros : matriz) {
+            for (int j = 0; j < casilleros.length; j++) {
+                sb.append(casilleros[j]);
                 sb.append(" ");
             }
             sb.append("\n");
